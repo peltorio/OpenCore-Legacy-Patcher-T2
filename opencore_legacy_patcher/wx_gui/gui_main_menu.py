fadc91b45d7ed1d12129d99c3b5a7661d91903d2
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-05-16 23:34:00 +0300

    Fix description key in menu options

--- opencore_legacy_patcher/wx_gui/gui_main_menu.py
+++ opencore_legacy_patcher/wx_gui/gui_main_menu.py
@@ -97,7 +97,7 @@ class MainFrame(wx.Frame):
             "Install drivers and patches": {
                 "function": self.on_post_install_root_patch,
                 "description": ["Installs hardware drivers and", "patches for your system after", "installing a new version of macOS."],
-                "description": ["This is the equivalent of the Install Root Patch button."],
+                "description2": ["This is the equivalent of the Install Root Patch button."],
                 "icon": str(self.constants.icns_resource_path / "OC-Patch.icns"),
             },
             "Support": {
